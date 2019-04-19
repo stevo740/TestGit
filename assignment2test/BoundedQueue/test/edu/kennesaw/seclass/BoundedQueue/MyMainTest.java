c4d48ef8df0b8bcba8f549bf123bfc3322aa46d8
steve anderson <stevo.7401@gmail.com>
2019-04-19 14:23:44 -0400

    test commit

---
+++ assignment2test/BoundedQueue/test/edu/kennesaw/seclass/BoundedQueue/MyMainTest.java
@@ -0,0 +1,17 @@
+package edu.kennesaw.seclass.BoundedQueue;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class MyMainTest {
+
+/*
+Place all  of your tests in this class.
+*/
+
+}
